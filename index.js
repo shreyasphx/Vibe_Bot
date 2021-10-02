214482e7de9b076d178fb1e1ba832f1cae96b526
shreyasphx <ssharmaphx@gmail.com>
2021-10-02 11:30:27 -0700

    added commands

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const{
 const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
+var queue = [];
+
 client.login(token);
 
 client.once('ready', () => {
@@ -21,6 +23,23 @@ client.once('disconnect', () => {
 client.on("message", async message => {
     if(message.author.bot){
         return;
+    }
+    if(message.content.charAt(0) == prefix){
+        if(message.content.substring(0,0) === "p"){
+
+        }
+        if(message.content.substring(0,0) === "s"){
+            
+        }
+        if(message.content.substring(0,0) === "q"){
+            
+        }
+        if(message.content.substring(0,1) === "np"){
+            
+        }
+        if(message.content.substring(0,0) === "k"){
+            
+        }
     }
     
 }) 
