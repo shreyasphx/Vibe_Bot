e169f1d42b150a1efab840253f9a8a2417a3f481
shreyasphx <ssharmaphx@gmail.com>
2021-10-02 13:47:07 -0700

    Implemented q and np functions and added comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const Discord = require(discord.js);
+//dependencies
+const Discord = require('discord.js');
 const{
     prefix,
     token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
-const voiceChannel = message.member.voice.channel;
 const client = new Discord.Client();
+//A queue of songs to be played
 var queue = [];
 
 client.login(token);
@@ -20,18 +21,13 @@ client.once('disconnect', () => {
     console.log('Disconnect!');
    });
 
+//interpret commands whenever a user sends a message
 client.on("message", async message => {
     if(message.author.bot){
         return;
     }
     if(message.content.charAt(0) == prefix){
-        const args = message.content.split(" ");
-        const songInfo = await ytdl.getInfo(args[1]);
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-        };
-        queue.push(song);
+        const voiceChannel = message.member.voice.channel;
         if(voiceChannel){
             message.author.channel.join();
             message.reply("Joined the voice channel!");
@@ -40,21 +36,45 @@ client.on("message", async message => {
             message.reply("User is not in voice channel!");
             return;
         }
-        if(message.content.substring(0,0) === "p"){
-           
-
+        if(message.content.substring(1,1) === "p"){
+            const args = message.content.split(" ");
+            const songInfo = await ytdl.getInfo(args[1]);
+            const song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url,
+            };
+            queue.push(song);
+            play();
         }
-        if(message.content.substring(0,0) === "s"){
+        if(message.content.substring(1,1) === "s"){
             
         }
-        if(message.content.substring(0,0) === "q"){
-            
+        if(message.content.substring(1,1) === "q"){
+        //sends info on all the items in the queue
+            var queueLog = "";
+            for(let i = 0; i < queue.length; i++){
+                queueLog += i + ". "
+                queueLog += queue[i].title + "\n*** ";
+                queueLog += queue[i].url + " ***" + "\n";
+            }
+            message.reply("There are " + queue.length + " items in the queue:\n" + queueLog);
         }
-        if(message.content.substring(0,1) === "np"){
-            
+        if(message.content.substring(1,2) === "np"){
+            //sends info on the currently playing item
+            message.reply("Now Playing: " + queue[0].title + "\n" + queue[0].url);
         }
-        if(message.content.substring(0,0) === "k"){
+        if(message.content.substring(1,1) === "k"){
             
         }
     }
 })
+
+//play the contents of the queue using youtubedl
+async function play(){
+
+}
+
+//stops whatever is being played
+async function stop(){
+
+}
