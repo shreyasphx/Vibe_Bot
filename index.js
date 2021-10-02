dd576dc3e989817149ae47749437e05ab2fcbf6d
mjberry4 <91344551+mjberry4@users.noreply.github.com>
2021-10-02 14:30:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const Discord = require(discord.js);
+global.Discord = require("discord.js");
 const{
     prefix,
     token,
-} = require('./config.json');
-const ytdl = require('ytdl-core');
-const voiceChannel = message.member.voice.channel;
+} = require("./config.json"); 
+const ytdl = require("ytdl-core");
 const client = new Discord.Client();
-var queue = [];
+const queue = [];
 
 client.login(token);
-
 client.once('ready', () => {
     console.log('Ready!');
    });
@@ -19,11 +17,12 @@ client.once('reconnecting', () => {
 client.once('disconnect', () => {
     console.log('Disconnect!');
    });
-
 client.on("message", async message => {
     if(message.author.bot){
         return;
     }
+
+    const voiceChannel = message.member.voice.channel;
     if(message.content.charAt(0) == prefix){
         const args = message.content.split(" ");
         const songInfo = await ytdl.getInfo(args[1]);
@@ -33,28 +32,70 @@ client.on("message", async message => {
         };
         queue.push(song);
         if(voiceChannel){
-            message.author.channel.join();
+            voiceChannel.join();
             message.reply("Joined the voice channel!");
         }
         else{
             message.reply("User is not in voice channel!");
             return;
         }
-        if(message.content.substring(0,0) === "p"){
-           
+        if(message.content.charAt(1) === 'p'){
+            
+            const serverQueue = queue.getInfo(guild.id);
+            if (!song) {
+                serverQueue.voiceChannel.leave();
+                queue.delete(guild.id);
+                return;
+            }
 
+            const stream = ytdl(queue.url, {filter: 'audioonly'});
+            const connection= await voiceChannel.join();
+            connection.play(stream, {seek:0, volume:5});
         }
+        
         if(message.content.substring(0,0) === "s"){
             
+
         }
         if(message.content.substring(0,0) === "q"){
-            
+          
+          quit(message, serverQueue);
+          return;     
         }
         if(message.content.substring(0,1) === "np"){
             
         }
         if(message.content.substring(0,0) === "k"){
-            
+
         }
+
     }
-})
+
+}
+)
+
+function play(guild, song) {
+
+    const serverQueue = queue.getInfo(guild.id);
+    if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+    }
+
+    ytdl(song.url);
+}
+
+function quit(guild, song) {
+
+    if (!message.member.voice.channel) {
+
+        return message.channel.send("You are not in a voice channel.");
+    } else if (!serverQueue) {
+
+            return message.channel.channel.send("There are no songs playing.");
+    }
+
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+}
